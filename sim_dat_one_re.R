56c5b553d8d29ea070e7215b8b233d6680feba41
yizhou fei <yizhou.fei@cuanschutz.edu>
2024-09-25 20:17:32 -0600

    Update code for one re

---
+++ sim_dat_one_re.R
@@ -0,0 +1,99 @@
+library(MASS)
+library(survival)
+library(tidyr)
+library(dplyr)
+dirg <- "C:/Users/feiyi/OneDrive/Desktop/Katie/Terminal-Decline"
+setwd(dirg)
+num_simulations <- 100
+# Simulation loop
+for (sim in 1:num_simulations) {
+  set.seed(123 + sim)  
+  # Parameters
+  cluster <- 40
+  cluster_subj <- 20
+  n <- cluster * cluster_subj
+  time <- 12
+  alpha00 <- 10
+  alpha01 <- 5
+  alpha02 <- 0.3
+  alpha03 <- 2 
+  alpha04 <- 10 
+  alpha11 <- 1
+  alpha12 <- 0.05 
+  #alpha13 <- 2
+  
+  b <- 0.03
+  #c <- 1
+  lambda0 <- 0.05
+  gamma <- 1.2
+  #sigma_u <- 1
+  sigma_b <- 8
+  sigma_e <- 3
+  
+  # Fixed effects covariates
+  x1 <- rbinom(n, 1, 0.5)
+  x2 <- rnorm(n, mean = 70, sd = 10)
+  x2 <- x2-mean(x2)
+  time_points <- seq(0,time, by=3)
+  subject_cluster <- rep(1:cluster, each = cluster_subj)
+  treatment_clusters <- sample(1:cluster, size = cluster/2, replace = FALSE)
+  treatment <- ifelse(subject_cluster %in% treatment_clusters, 0, 1)
+  
+  # Random effects
+  ui <- rnorm(cluster, mean = 0, sd = sigma_u)
+  bi <- rnorm(n, mean = 0, sd = sigma_b)
+  epsiloni <- mvrnorm(n, mu = rep(0, time), Sigma = diag(sigma_e*sigma_e, time))
+  
+  # Cox frailty model for survival data assuming Weibull distribution
+  linear <- alpha11 * x1 + alpha12 * x2  + b * bi 
+  lambda <- lambda0 * exp(linear)
+  U <- runif(n)
+  
+  # Simulate survival times
+  survival_times <- (-log(U)/lambda)^(1/gamma)
+  censoring_times <- time 
+  observed_times <- pmin(survival_times, censoring_times)
+  status <- as.numeric(survival_times <= censoring_times)
+  
+  # Mixed model for longitudinal data, modeling backward from death
+  longitudinal_data <- data.frame()
+  
+  for (i in 1:n) {
+    for (ind in seq_along(time_points)) {
+      t = time_points[ind]
+      backward_time <- survival_times[i] - t
+      if (backward_time > 0) {
+        measurement <- alpha00 + x1[i] * alpha01 + x2[i] * alpha02 + backward_time * alpha03 + treatment[i] * alpha04 + bi[i] + epsiloni[i, ind]
+      } else {
+        measurement <- NA
+      }
+      longitudinal_data <- rbind(longitudinal_data, data.frame(subject = i, time = t, measurement = measurement))
+    }
+  }
+  
+  # Join the longitudinal and survival data
+  longitudinal_data <- longitudinal_data %>%
+    left_join(data.frame(subject = 1:n, observed_time = observed_times), by = "subject") %>%
+    mutate(measurement = ifelse(time > observed_time, NA, measurement)) %>%
+    select(-observed_time)
+  
+  # Create a mask for the missing data
+  longitudinal_data_wide <- longitudinal_data %>%
+    pivot_wider(names_from = time, values_from = measurement, names_prefix = "time_")
+  mask <- !is.na(as.matrix(longitudinal_data_wide[, grep("time_", colnames(longitudinal_data_wide))]))
+  
+  longitudinal_data_wide[is.na(longitudinal_data_wide)] <- 0
+  
+  final_data <- longitudinal_data_wide %>%
+    left_join(data.frame(subject = 1:n, x1, x2, cluster = subject_cluster, treatment = treatment, time = observed_times, death = survival_times, status),
+              by = "subject")
+  
+  mask_df <- as.data.frame(mask)
+  filename <- paste0("mask0.", sim-1, ".csv")
+  write.csv(mask_df, file = filename, row.names = FALSE)
+  
+  filename <- paste0("sim0.data.", sim-1, ".csv")
+  write.csv(final_data, file = filename, row.names = FALSE)
+  
+}
+
---
+++ sim_one_re.R
@@ -0,0 +1,155 @@
+#!/usr/bin/env Rscript
+library(MASS)
+library(survival)
+library(tidyr)
+library(dplyr)
+library(rstan)
+
+mask <- as.matrix(read.csv(list.files(pattern="mask0.")))
+final_data <- as.data.frame(read.csv(list.files(pattern="sim0.data.")))
+time_points <- seq(0, 12, by = 3)
+
+stan_data <- list(
+  N = nrow(final_data),  # Total number of subjects
+  K = length(unique(final_data$cluster)),  # Number of clusters
+  T = length(grep("time_", names(final_data))),  # Number of time points
+  cluster = final_data$cluster,  # Cluster indicator
+  treatment = final_data$treatment,
+  time_points = time_points,
+  x1 = final_data$x1,  # Binary covariate
+  x2 = final_data$x2,  # Continuous covariate
+  y = as.matrix(final_data[, grep("time_", names(final_data))]),  # Longitudinal measurements
+  mask = mask,  # Mask for missing values
+  survival_time = final_data$time,  # Survival times
+  status = final_data$status  # Censoring indicator
+)
+
+
+
+# Compile the Stan model once
+stan_model_code <- "
+data {
+  int<lower=0> N;  // total number of subjects
+  int<lower=0> K;  // number of clusters
+  int<lower=0> T;  // number of time points
+  int<lower=1,upper=K> cluster[N];  // cluster indicator
+  vector[N] x1;  // binary covariate
+  vector[N] x2;  // continuous covariate
+  vector[N] treatment;
+  vector[T] time_points;
+  matrix[N, T] y;  // longitudinal measurements
+  matrix[N, T] mask;  // mask for missing values
+  real<lower=0> survival_time[N];  // observed survival or censoring times
+  int<lower=0,upper=1> status[N];  // censoring indicator
+}
+parameters {
+  real alpha00;
+  real alpha01;
+  real alpha02;
+  real alpha03;
+  real alpha04;
+  real alpha11;
+  real alpha12;
+
+  real b;
+
+  real<lower=0> sigma_b;
+
+  real<lower=0> sigma_e;
+  real<lower=0> lambda0;
+  real<lower=0> gamma;
+  
+  vector[N] z_b;  
+
+  vector<lower=0,upper=1>[N] U;
+  
+}
+transformed parameters {
+  
+  vector[N] lambda;
+  vector[N] death_time;
+  vector[N]  b_i = z_b * sigma_b;
+
+  
+  for (i in 1:N) {
+    lambda[i] = lambda0 * exp(alpha11 * x1[i] + alpha12 * x2[i] + b * b_i[i]);
+  }
+   
+  
+  for (i in 1:N) {
+    if (status[i] == 1) {
+      death_time[i] = survival_time[i];  // use observed death time for uncensored
+    } else {
+      death_time[i] = pow(-log(exp(-lambda * pow(survival_time[i], gamma))-U[i] *exp(-lambda * pow(survival_time[i], gamma))) / lambda, 1 / gamma);
+    }
+  }
+}
+
+model {
+
+  // Priors
+  alpha00 ~ normal(0, 10);
+  alpha01 ~ normal(0, 10);
+  alpha02 ~ normal(0, 10);
+  alpha03 ~ normal(0, 10);
+  alpha04 ~ normal(0, 10);
+  alpha11 ~ normal(0, 10);
+  alpha12 ~ normal(0, 10);
+
+  b ~ normal(0, 1);
+
+
+  sigma_b ~ normal(0, 10);
+  sigma_e ~ normal(0, 10);
+  lambda0 ~ inv_gamma(2, 1);
+  gamma ~ gamma(0.5, 0.5);
+
+  z_b ~ normal(0, 1);
+
+
+  // Longitudinal model
+  for (i in 1:N) {
+    for (t in 1:T) {
+      real backward_time = death_time[i] - time_points[t];
+      if (mask[i, t] > 0) {
+        y[i, t] ~ normal(alpha00 + x1[i] * alpha01 + x2[i] * alpha02 + backward_time * alpha03 + treatment[i] * alpha04 + b_i[i], sigma_e);
+      }
+    }
+  }
+  
+    
+  // Survival likelihood
+ for (i in 1:N) {
+    if (status[i] == 1) {
+      // Observed death time: use weibull exponential likelihood
+      target += log(lambda) + log(gamma) + (gamma - 1) * log(death_time[i])  - lambda * death_time[i]^gamma;
+    } else {
+      // Censored event: Weibull survival function
+      target += -lambda * survival_time[i]^gamma;
+    }
+  }
+}
+"
+stan_model <- stan_model(model_code = stan_model_code)
+
+init_fn <- function() {
+  list(alpha00 = 10, alpha01 = 5, alpha02 = 0.3, alpha03 = 2, alpha04 = 10, alpha11 = 1, alpha12 = 0.05, b = 0.03, lambda0 = 0.05, gamma = 1.2, sigma_b = 8, sigma_e = 3)
+}
+
+# Compile and sample from the Stan model
+fit <- sampling(stan_model, data = stan_data, init = init_fn, iter = 4000, warmup = 2000, chains = 2, control = list(adapt_delta = 0.99, max_treedepth = 12), cores=2)
+
+result <- summary(fit)
+fit_df <- as.data.frame(result$summary)
+text <- list.files(pattern="sim0.data.")
+num <- unlist(lapply(strsplit(text,'.',fixed=TRUE),function(x) x[[3]]))
+write.csv(fit_df, paste0("mod0.result.",num,".csv"))
+
+
+pdf(file = paste0("mod0.traceplot.",num,".pdf"),   # The directory you want to save the file in
+    width = 4, # The width of the plot in inches
+    height = 4) # The height of the plot in inches
+traceplot(fit, c("alpha00","alpha01","alpha02","alpha03","alpha04","alpha11","alpha12","b","c","lambda0","gamma","sigma_b","sigma_u","sigma_e"))
+dev.off()
+
+
